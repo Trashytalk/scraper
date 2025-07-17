cde0e389a5406ec45aeb93c612ddead4c58d02c5
Trashytalk <trashemaillll123@gmail.com>
2025-07-17 17:25:29 -0400

    Add proxy pool manager

--- business_intel_scraper/backend/proxy/__init__.py
+++ business_intel_scraper/backend/proxy/__init__.py
@@ -2,10 +2,12 @@
 
 from .manager import ProxyManager
 from .provider import ProxyProvider, DummyProxyProvider, APIProxyProvider
+from .proxy_manager import ProxyPoolManager
 
 __all__ = [
     "ProxyManager",
     "ProxyProvider",
     "DummyProxyProvider",
     "APIProxyProvider",
+    "ProxyPoolManager",
 ]
