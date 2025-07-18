e56695a3a234a78340a477ecd64e70a057a536d3
Trashytalk <trashemaillll123@gmail.com>
2025-07-17 20:37:14 -0400

    Add regional spider stubs

--- business_intel_scraper/backend/crawlers/__init__.py
+++ business_intel_scraper/backend/crawlers/__init__.py
@@ -7,7 +7,64 @@ except ModuleNotFoundError:  # pragma: no cover - optional dependency
 
 from .browser import BrowserCrawler
 from .playwright_utils import fetch_with_playwright
+from .spiders.southeast_asia import (
+    AseanPepCorruptionCaseSpider,
+    CambodiaMinistryOfCommerceFilingsSpider,
+    IndonesiaOJKEnforcementSpider,
+    MalaysiaSCDisclosureSpider,
+    PhilippinesPhilgepsProcurementSpider,
+    RegionalTaxTribunalJudgmentSpider,
+    SeAsiaRenewableEnergyCarbonCreditRegistrySpider,
+    SingaporeMASEnforcementActionsSpider,
+    SingaporeStraitsTimesMalaysianStarCorporateNewsSpider,
+    ThailandBoardOfInvestmentSpider,
+    VietnamExportProcessingZoneRegistrySpider,
+    VietnamStateSecuritiesCommissionPenaltySpider,
+)
+from .spiders.middle_east import (
+    AbuDhabiDubaiFreeZoneCompanyListSpider,
+    DubaiCourtsLitigationAnnouncementSpider,
+    DubaiLandDepartmentTitleDeedSpider,
+    EgyptPublicBusinessSectorCompanyReportsSpider,
+    GCCTaxAuthoritySpider,
+    GCCTenderBlacklistAnnouncementsSpider,
+    IranChamberOfCommerceMembershipSpider,
+    IsraelInnovationAuthorityGrantsSpider,
+    JordanCompaniesControlDepartmentRegistrySpider,
+    KuwaitSaudiPublicProcurementSpider,
+    QatarCentralBankAMLEnforcementSpider,
+    SaudiNitaqatComplianceSpider,
+    TurkeyKAPPublicDisclosurePlatformSpider,
+)
 
 __all__ = ["BrowserCrawler", "fetch_with_playwright"]
 if ExampleSpider is not None:
     __all__.insert(0, "ExampleSpider")
+
+__all__ += [
+    "SingaporeStraitsTimesMalaysianStarCorporateNewsSpider",
+    "SingaporeMASEnforcementActionsSpider",
+    "ThailandBoardOfInvestmentSpider",
+    "VietnamExportProcessingZoneRegistrySpider",
+    "IndonesiaOJKEnforcementSpider",
+    "MalaysiaSCDisclosureSpider",
+    "CambodiaMinistryOfCommerceFilingsSpider",
+    "AseanPepCorruptionCaseSpider",
+    "RegionalTaxTribunalJudgmentSpider",
+    "SeAsiaRenewableEnergyCarbonCreditRegistrySpider",
+    "PhilippinesPhilgepsProcurementSpider",
+    "VietnamStateSecuritiesCommissionPenaltySpider",
+    "DubaiLandDepartmentTitleDeedSpider",
+    "GCCTaxAuthoritySpider",
+    "AbuDhabiDubaiFreeZoneCompanyListSpider",
+    "KuwaitSaudiPublicProcurementSpider",
+    "JordanCompaniesControlDepartmentRegistrySpider",
+    "QatarCentralBankAMLEnforcementSpider",
+    "IsraelInnovationAuthorityGrantsSpider",
+    "IranChamberOfCommerceMembershipSpider",
+    "EgyptPublicBusinessSectorCompanyReportsSpider",
+    "SaudiNitaqatComplianceSpider",
+    "GCCTenderBlacklistAnnouncementsSpider",
+    "TurkeyKAPPublicDisclosurePlatformSpider",
+    "DubaiCourtsLitigationAnnouncementSpider",
+]
---
+++ business_intel_scraper/backend/crawlers/spiders/__init__.py
@@ -0,0 +1,1 @@
+"""Regional Scrapy spider definitions."""
